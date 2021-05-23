d54a3b73c7d640d89d680474e7d10a2f6be1c433
Jimon <22317839+Jimon-s@users.noreply.github.com>
2021-05-23 18:27:15 +0900

    Migrate Lambda - Editor, Catalog, Eraser to Terraform

--- terraform/env/example/main.tf
+++ terraform/env/example/main.tf
@@ -16,3 +16,42 @@ module "lambda_register" {
   dynamodb_table_name           = module.dymanodb_tools_table.dynamodb_table_name
   dynamodb_table_arn            = module.dymanodb_tools_table.dynamodb_table_arn
 }
+
+module "lambda_editor" {
+  source = "../../module/lambda_official_tool"
+
+  function_name                 = "SlackHubEditor"
+  source_code_dir               = "../../../editor/bin"
+  source_code_file              = "main"
+  region                        = data.aws_region.current.name
+  param_key_bot_user_auth_token = local.param_key_bot_user_auth_token
+  param_key_signing_secret      = local.param_key_signing_secret
+  dynamodb_table_name           = module.dymanodb_tools_table.dynamodb_table_name
+  dynamodb_table_arn            = module.dymanodb_tools_table.dynamodb_table_arn
+}
+
+module "lambda_catalog" {
+  source = "../../module/lambda_official_tool"
+
+  function_name                 = "SlackHubCatalog"
+  source_code_dir               = "../../../catalog/bin"
+  source_code_file              = "main"
+  region                        = data.aws_region.current.name
+  param_key_bot_user_auth_token = local.param_key_bot_user_auth_token
+  param_key_signing_secret      = local.param_key_signing_secret
+  dynamodb_table_name           = module.dymanodb_tools_table.dynamodb_table_name
+  dynamodb_table_arn            = module.dymanodb_tools_table.dynamodb_table_arn
+}
+
+module "lambda_eraser" {
+  source = "../../module/lambda_official_tool"
+
+  function_name                 = "SlackHubEraser"
+  source_code_dir               = "../../../eraser/bin"
+  source_code_file              = "main"
+  region                        = data.aws_region.current.name
+  param_key_bot_user_auth_token = local.param_key_bot_user_auth_token
+  param_key_signing_secret      = local.param_key_signing_secret
+  dynamodb_table_name           = module.dymanodb_tools_table.dynamodb_table_name
+  dynamodb_table_arn            = module.dymanodb_tools_table.dynamodb_table_arn
+}
